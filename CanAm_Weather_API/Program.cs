82af98fe1b9579e6cdab3fa62fe423a15d5b65d3
Thomas Theodorescu <twtheo4@live.com>
2024-01-09 15:35:10 -0700

    Initial run - retrieving data successfully

--- CanAm_Weather_API/Program.cs
+++ CanAm_Weather_API/Program.cs
@@ -2,13 +2,40 @@
 
 namespace CanAm_Weather_API
 {
+
+    /*
+        
+        Software Engineering staff at Can/Am are often tasked with developing integrations against a real-time API. In this exercise, you will write a simple application that retrieves weather forecast information for a particular zone or weather station and presents it in a nicely formatted way. Here are the key requirements:
+
+        1. Use data from the National Weather Service API: 
+        https://www.weather.gov/documentation/services-web-api#/default/office
+        2. Allow the user to input a State (e.g., CO) and retrieve a list of local entities - e.g., zones, weather stations - from which forecast data can be accessed.
+        3. Present a detailed forecast based on a specific local entity or set of coordinates. You can decide what API service to use to pull forecast information, as well as how to present your data; there are several options available in the API. However, you should present the data in a way that makes it easy to see hourly or daily forecasts with temperature highs/lows.
+        4. Feel free to use the programming language of your choice - C#, Python, etc.
+
+        Once you are done, send me your source code and the amount of time you spent on the exercise.
+     */
     class Program
     {
-        private static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Program Start");
+            try
+            {
+                Console.WriteLine("Please enter a state using it's state code eg. 'CO' for Colorado: ");
+                string state = Console.ReadLine();
+                Console.WriteLine(state);
 
-            new WeatherDataRetrieval weatherDataRetrieval = new WeatherDataRetrieval();
+                WeatherDataRetrieval weatherDataRetrieval = new WeatherDataRetrieval();
+
+                var result = await weatherDataRetrieval.GetEntitiesAsync(state);
+
+                Console.WriteLine("end Test");
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Exception encountered!");
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }
--- CanAm_Weather_API/WeatherDataRetrieval.cs
+++ CanAm_Weather_API/WeatherDataRetrieval.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,44 @@ namespace CanAm_Weather_API
     {
         private int _testInt;
         private string _testStr;
+        private HttpClient _httpClient = new HttpClient();
 
 
-        public WeatherDataRetrieval(int testINt, string testStr) 
+        public WeatherDataRetrieval() 
         {
-            _testInt = testINt;
-            _testStr = testStr;
+            _testInt = 0;
+            _testStr = "test";
+
+            //valid state codes
+            //[ AL, AK, AS, AR, AZ, CA, CO, CT, DE, DC, FL, GA, GU, HI, ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH, NJ, NM, NY, NC, ND, OH, OK, OR, PA, PR, RI, SC, SD, TN, TX, UT, VT, VI, VA, WA, WV, WI, WY, MP, PW, FM, MH ]
+        }
+
+        public async Task<string> GetEntitiesAsync(string state)
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, "https://api.weather.gov/stations?state=CO&limit=500");
+
+                var appValue = new ProductInfoHeaderValue("CanAm_Weather_Exercise", "1.0");
+                var emailValue = new ProductInfoHeaderValue("(+CanAmWeatherExercise@gmail.com)");
+
+                request.Headers.UserAgent.Add(appValue);
+                request.Headers.UserAgent.Add(emailValue);
+
+                var response = await _httpClient.SendAsync(request);
+
+                //HttpResponseMessage response = await _httpClient.GetAsync("https://api.weather.gov/stations?state=CO&limit=500");
+
+                Console.WriteLine("Content: " + response.Content);
+
+                return response.Content.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return "failed";
         }
 
 
