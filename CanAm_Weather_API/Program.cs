1c1a97ed002d89cc2174726e4edd3a49899eb23b
Thomas Theodorescu <Thomas Theodorescu@THOMAS-PC>
2024-01-09 14:30:17 -0700

    Add project files.

---
+++ CanAm_Weather_API/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace CanAm_Weather_API
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Program Start");
+
+            new WeatherDataRetrieval weatherDataRetrieval = new WeatherDataRetrieval();
+
+        }
+    }
+}
---
+++ CanAm_Weather_API/WeatherDataRetrieval.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanAm_Weather_API
+{
+    public class WeatherDataRetrieval
+    {
+        private int _testInt;
+        private string _testStr;
+
+
+        public WeatherDataRetrieval(int testINt, string testStr) 
+        {
+            _testInt = testINt;
+            _testStr = testStr;
+        }
+
+
+    }
+}
